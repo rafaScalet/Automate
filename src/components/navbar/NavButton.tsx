a174a246925cf6b2121d98c5e9c03f114ce7b0a2
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-12-03 16:36:35 -0300

    fix: better tailwind usage

--- src/components/navbar/NavButton.tsx
+++ src/components/navbar/NavButton.tsx
@@ -11,7 +11,7 @@ export function NavButton({ to, children, icon }: NavButtonProps) {
   return (
     <NavLink
       className={({ isActive }) =>
-        `flex items-center justify-center gap-1 rounded-md p-2 align-middle ${isActive ? 'bg-neutral-300 dark:bg-zinc-800' : ''}`
+        `flex items-center justify-center gap-1 rounded-md p-2 align-middle ${isActive && 'bg-neutral-300 dark:bg-zinc-800'}`
       }
       to={to}
     >
