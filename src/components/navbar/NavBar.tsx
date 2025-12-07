76072d40e68b6b6e0ed6540868c62d2d28c83318
Murilo Santos <151096774+muriisantuss@users.noreply.github.com>
2025-12-07 11:24:26 -0300

    :sparkles: feat: update navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ export function NavBar() {
   return (
     <nav className="sticky top-0 z-50 flex w-full gap-6 bg-neutral-200 p-3 dark:bg-zinc-900">
       <NavButton to="/">Dashboard</NavButton>
-      <NavButton to="/new">New Trash</NavButton>
+      <NavButton to="/new">Cadastrar Lixeira</NavButton>
     </nav>
   );
 }
