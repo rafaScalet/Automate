ece4a5ec37b3e7c416dd601e2f5339c5fde52a7e
Murilo Santos <151096774+muriisantuss@users.noreply.github.com>
2025-12-07 11:27:10 -0300

    :sparkles: feat: update firebase-config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoKkjXuo7X76hlkssJ80W5GeIMYEKvCQ4",
-  authDomain: "automate-52c0d.firebaseapp.com",
-  databaseURL: "https://automate-52c0d-default-rtdb.firebaseio.com",
-  projectId: "automate-52c0d",
-  storageBucket: "automate-52c0d.firebasestorage.app",
-  messagingSenderId: "432746830582",
-  appId: "1:432746830582:web:b5fed02922c2b6491a2ad7"
+  apiKey: 'AIzaSyAoKkjXuo7X76hlkssJ80W5GeIMYEKvCQ4',
+  authDomain: 'automate-52c0d.firebaseapp.com',
+  databaseURL: 'https://automate-52c0d-default-rtdb.firebaseio.com',
+  projectId: 'automate-52c0d',
+  storageBucket: 'automate-52c0d.firebasestorage.app',
+  messagingSenderId: '432746830582',
+  appId: '1:432746830582:web:b5fed02922c2b6491a2ad7',
 };
 
 // Inicializa o Firebase
