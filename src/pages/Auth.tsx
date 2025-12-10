577ba985e3b89463c05766d0d35984ec7e4bc784
yuuyaokatani <yuuyaokatani@gmail.com>
2025-12-09 22:39:18 -0300

    :sparkles: feat: Added show/hide password buttton

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,10 +5,12 @@ import { Card } from '@/components/ui/Card';
 import { Input } from '@/components/ui/Input';
 import { Loading } from '@/components/ui/Loading';
 import { notify } from '@/components/ui/notify';
+import { Icon } from '@iconify/react';
 
 export function Auth({ children }: { children?: React.ReactNode }) {
   const { user, loginWithEmail } = useAuth();
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -46,15 +48,24 @@ export function Auth({ children }: { children?: React.ReactNode }) {
                 onChange={(e) => setForm({ ...form, email: e.target.value })}
                 required
               />
-              <Input
-                id="password"
-                label="Senha:"
-                placeholder="Digite sua senha"
-                type="password"
-                value={form.password}
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
-                required
-              />
+              <div className="relative">
+                <Input
+                  id="password"
+                  label="Senha:"
+                  placeholder="Digite sua senha"
+                  type={showPassword ? 'text' : 'password'}
+                  value={form.password}
+                  onChange={(e) => setForm({ ...form, password: e.target.value })}
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-9 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <Icon icon={showPassword ? 'lucide:eye-off' : 'lucide:eye'} className="h-5 w-5" />
+                </button>
+              </div>
 
               <Button type="submit" className="w-full" loading={loading}>
                 Entrar
